c959d5ab405e98b416c748b458559b7334221f9c
Alexis Bassignot <misterbalise2@gmail.com>
2023-09-18 11:55:41 +0200

    Installation BDD

---
+++ src/DataFixtures/EtudiantFixtures.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Etudiant;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class EtudiantFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        for ($i = 0; $i < 10; $i++) {
+            $etudiant = new Etudiant();
+            $etudiant->setPrenom("Joachim$i");
+            $manager->persist($etudiant);
+        }
+        $manager->flush();
+    }
+}
