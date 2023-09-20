02588c738e0facf8ace1db8017832f808963d016
Alexis Bassignot <misterbalise2@gmail.com>
2023-09-20 08:59:46 +0200

    Modif des fixtures PromotionFixtures.php

--- src/DataFixtures/PromotionFixtures.php
+++ src/DataFixtures/PromotionFixtures.php
@@ -2,15 +2,22 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Promotion;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class PromotionFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-
-
+        $faker = Factory::create('FR_fr');
+        for ($i=0;$i<10;$i++) {
+            $promotion = new Promotion();
+            $promotion->setNom($faker->unique()->word());
+            $promotion->setAnnee("2023");
+            $manager->persist($promotion);
+        }
         $manager->flush();
     }
 }
