3b989aee7671334d9a390ab127874dbb8d8ad0d0
Alexis Bassignot <misterbalise2@gmail.com>
2023-09-20 09:53:04 +0200

    Fin de la seance

--- src/Controller/PromotionController.php
+++ src/Controller/PromotionController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Repository\PromotionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PromotionController extends AbstractController
 {
-    #[Route('/promotion', name: 'app_promotion')]
-    public function index(): Response
+    #[Route('/promotion', name: 'app_promotion_index')]
+    public function index(PromotionRepository $promotionRepository): Response
     {
+        $listePromotion = $promotionRepository->findAll();
+//        $nombreEleves = count($listePromotion);
         return $this->render('promotion/index.html.twig', [
-            'controller_name' => 'PromotionController',
+            'promotions' => $listePromotion
+        ]);
+    }
+    #[Route('/promotion/{id}', name: 'app_promotion_fiche', requirements: ['id' => '\d+'])]
+    public function fiche(PromotionRepository $promotionRepository, $id): Response
+    {
+        $Promotion = $promotionRepository->find($id);
+//        $nombreEleves = count($listePromotion);
+        return $this->render('promotion/fiche.html.twig', [
+            'promotion' => $Promotion
         ]);
     }
 }
