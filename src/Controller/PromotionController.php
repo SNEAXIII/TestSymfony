d77292da07bcac725fe5b3842f8872391f7784a3
AlexisB <misterbalise2@gmail.com>
2023-09-24 22:56:26 +0200

    Ajout du nombre d'étudiant par formation et de la liste des étudiant dans la méthode fiche de PromotionController.php

--- src/Controller/PromotionController.php
+++ src/Controller/PromotionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\EtudiantRepository;
 use App\Repository\PromotionRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -12,19 +13,24 @@ class PromotionController extends AbstractController
     #[Route('/promotion', name: 'app_promotion_index')]
     public function index(PromotionRepository $promotionRepository): Response
     {
-        $listePromotion = $promotionRepository->findAll();
+        $listePromotion = $promotionRepository -> findAll();
 //        $nombreEleves = count($listePromotion);
-        return $this->render('promotion/index.html.twig', [
+        return $this -> render('promotion/index.html.twig', [
             'promotions' => $listePromotion
         ]);
     }
+
     #[Route('/promotion/{id}', name: 'app_promotion_fiche', requirements: ['id' => '\d+'])]
-    public function fiche(PromotionRepository $promotionRepository, $id): Response
+    public function fiche(PromotionRepository $promotionRepository, EtudiantRepository $etudiantRepository, $id): Response
     {
-        $Promotion = $promotionRepository->find($id);
-//        $nombreEleves = count($listePromotion);
-        return $this->render('promotion/fiche.html.twig', [
-            'promotion' => $Promotion
+        $Promotion = $promotionRepository -> find($id);
+        $ListEtudiant = $etudiantRepository -> findByPromotion($id);
+        $nombreEleves = count($ListEtudiant);
+        return
+            $this -> render('promotion/fiche.html.twig', [
+                'promotion' => $Promotion,
+                'liste' => $ListEtudiant,
+                "nombreEleves" => $nombreEleves
         ]);
     }
 }
